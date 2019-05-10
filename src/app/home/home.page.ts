dd890df6f2d30938bbd865e601cbf9f9bfbea432
Tairone Dias <taironecdias@gmail.com>
2019-05-10 11:06:00 -0300

    fd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,7 +11,7 @@ export class HomePage implements OnInit {
 
   private config;
 
-  constructor(public router: Router, 
+  constructor(public router: Router,
               private configService: ConfigSubjectService) {}
 
   ngOnInit() {
